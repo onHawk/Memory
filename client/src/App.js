c9da2d6cf756586985237da4774dc33c19b52c22
onHawk <luisd824@gmail.com>
2018-12-08 17:03:46 -0500

    add routes to landing and login pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './sass/App.scss';
+
+import Landing from './components/Landing';
+import Login from './components/accounts/Login';
+
+import HomePage from './components/Home';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route path="/login" component={Login} />
+          <Route path="/home" component={HomePage} />
+        </Switch>
+      </Router>
     );
   }
 }
 
 export default App;
+/* 
+ * 
+  <div className="App">
+  <header className="App-header">
+  <img src={logo} className="App-logo" alt="logo" />
+  </header>
+  </div> 
+ * 
+*/
