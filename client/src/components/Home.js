994df9032de85a646de1cd003a3e12e07c5b82a3
onHawk <luisd824@gmail.com>
2018-12-08 17:20:05 -0500

    placeholder homepage when logged in

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Home page</h1>
+      </div>
+    );
+  }
+}
+export default HomePage;
