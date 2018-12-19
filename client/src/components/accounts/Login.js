9e28dc49ae602472b2446e1b315925a849907b1b
onHawk <luisd824@gmail.com>
2018-12-19 14:55:05 -0500

    style login page

--- client/src/components/accounts/Login.js
+++ client/src/components/accounts/Login.js
@@ -13,16 +13,25 @@ class Login extends Component {
   }
   render() {
     return (
-      <div>
+      <div className="container">
+        <h2 style={{ margin: '0 0 3rem 0' }}>Login</h2>
         {this.props.error ? <h3>{this.props.error}</h3> : null}
-        <Form onSubmit={values => this.handleSubmit(values)}>
-          <label>username: </label>
-          <Text field="username" />
-          <label>password: </label>
-          <Text field="password" />
-          <button type="submit">Submit</button>
+        <Form onSubmit={values => this.handleSubmit(values)} className="form">
+          <div className="form-fields">
+            <label>username: </label>
+            <Text field="username" />
+          </div>
+          <div className="form-fields">
+            <label>password: </label>
+            <Text field="password" type="password" />
+          </div>
+
+          <button type="submit" className="form-button">
+            Submit
+          </button>
         </Form>
-        <Link to="/register">
+
+        <Link to="/register" className="redirect">
           <p>register</p>
         </Link>
       </div>
