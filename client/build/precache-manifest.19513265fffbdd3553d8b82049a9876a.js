4662b31becdb5461994d0f3da9f9fa0e8fe9456b
onHawk <luisd824@gmail.com>
2018-12-20 15:06:12 -0500

    add build for deploy

---
+++ client/build/precache-manifest.19513265fffbdd3553d8b82049a9876a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d24162e29bd3028b74d8",
+    "url": "/static/js/main.d24162e2.chunk.js"
+  },
+  {
+    "revision": "6636124026fc6fe61231",
+    "url": "/static/js/1.66361240.chunk.js"
+  },
+  {
+    "revision": "d24162e29bd3028b74d8",
+    "url": "/static/css/main.02bce7f0.chunk.css"
+  },
+  {
+    "revision": "258f29763c3e1b6cfa249eb4a6ec8e7f",
+    "url": "/index.html"
+  }
+];
