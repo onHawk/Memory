d1554ff87b26565db4384cac03b041b937baf6ba
onHawk <luisd824@gmail.com>
2018-12-22 18:57:10 -0500

    add link to home page on nav

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -38,11 +38,13 @@ class HomePage extends Component {
       <div className="homepage">
         <div className="nav">
           <h2>{this.props.user.username}</h2>
-
+          <Link to="/home" className="nav-link">
+            <p>home</p>
+          </Link>
           <p onClick={() => this.props.logout(this.props.history)}>Log out</p>
 
           <Link to="/create" style={{ textDecoration: 'none' }}>
-            <div className="nav-create">
+            <div className="nav-link">
               <FaFeatherAlt />
               <p>create</p>
             </div>
