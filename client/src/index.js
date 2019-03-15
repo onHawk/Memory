3aaed847f16a09144c103a6bad82b566e2d779cc
onHawk <luisd824@gmail.com>
2019-03-15 13:47:18 -0400

    add redux persist to app

--- client/src/index.js
+++ client/src/index.js
@@ -4,24 +4,41 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import rootReducer from './reducers/index';
+import autoMergeLevel1 from 'redux-persist/es/stateReconciler/autoMergeLevel1';
 
 require('./sass/main.scss');
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['journal'],
+  stataeReconciler: autoMergeLevel1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = createStore(
-  rootReducer,
+  persistedReducer,
   composeEnhancers(applyMiddleware(ReduxThunk))
 );
 
+const persisted = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persisted}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
